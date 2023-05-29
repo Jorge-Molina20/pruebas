7c9c32a75bf4204b94aaff635fd7bf02b714ce5d
jorgeM20 <jm0241895@gmail.com>
2023-05-29 10:31:17 -0500

    29 mayo

--- src/App.js
+++ src/App.js
@@ -1,23 +1,84 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useEffect, useState } from 'react'
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
+import Layouts from './layouts/Layouts'
+import Dashboard from './views/Dashboard';
+import Encuestas from './views/Encuestas';
+import IPN from './views/IPN';
+import Reporte from './views/Reporte';
+import CrearEncuestas from './views/IPN/CrearEncuestas';
+import Create from './views/Create';
+import Login from './components/master/Login';
+import globalServices from './services/global'
 
 function App() {
+  const [user, setUser] = useState(localStorage.getItem('loggedUser'))
+  const [load, setLoad] = useState(false)
+  useEffect(() => {
+    const loggedUserJson = localStorage.getItem('loggedUser')
+    if (loggedUserJson) {
+      const user = JSON.parse(loggedUserJson)
+      setUser(user)
+    }
+    verificaToken()
+  }, [])
+
+  const verificaToken = async () => {
+    await globalServices.verificaToken()
+    setTimeout(() => {
+      verificaToken()
+    }, 600000)
+    setLoad(true)
+    console.log('cargar')
+  }
+  
+  setTimeout(function () {
+    refreshTokenVerifyInactivity()
+  }, 1000 * 60 * 60)
+
+  const refreshTokenVerifyInactivity = async (event) => {
+    const resp = await globalServices.refreshTokenInactivity({})
+    if (resp.expire_date) {
+      await globalServices.logOut({})
+    }
+  }
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      {
+        user === null
+      ? <Router>
+          <Routes>
+            <Route path="/*" caseSensitive={false} element={<Login />} />
+          </Routes>
+        </Router>
+      : load &&
+      <Router>
+        <Routes>
+          <Route path="/dashboard/*" element={<Layouts />} >
+            <Route index element={<Dashboard />} />
+          </Route>
+
+          <Route path="/encuesta/*" element={<Layouts />} >
+            <Route index element={<Encuestas />} />
+          </Route>
+
+          <Route path="/ipn/*"element={<Layouts />} >
+            <Route index element={<IPN />} />
+          </Route>
+
+          <Route path="/reportes/*" element={<Layouts />} >
+            <Route index element={<Reporte />} />
+          </Route>
+
+          <Route path="ipn/crearEncuesta" element={<Layouts />} >
+            <Route index element={<CrearEncuestas />} />
+          </Route>
+
+          <Route path="/create" element={<Layouts />} >
+            <Route index element={<Create />} />
+          </Route>
+        </Routes>
+      </Router>
+      }
     </div>
   );
 }
---
+++ src/views/Encuestas.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import '../styles/encuestas.css'
+
+const Encuestas = () => {
+  return (
+    <div>
+      <h1 className='titulo-e' style={{ color: 'darkblue' }}>
+        Mis Encuestas
+      </h1>
+    </div>
+  )
+}
+
+export default Encuestas
